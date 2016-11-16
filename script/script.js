e861597342148c488f6a51d38d827c3c5f3e8075
= <scottmichaeldavey@gmail.com>
2016-11-15 22:40:09 -0500

    New BG images and beginnings of nav animations

--- script/script.js
+++ script/script.js
@@ -1,13 +1,22 @@
 
 var main = {
 	init: function () {
-		/*var wrapper, windowHeight, windowWidth, navHeight;
+		var wrapper, windowHeight, windowWidth, navHeight, navItem;
 		wrapper = $('#wrapper');
 		windowHeight = parseInt($(window).height(), 10);
 		windowWidth = parseInt($(window).width(), 10);
-		navHeight = 34;
-		wrapper.height('1000px');	//windowHeight - navHeight + 'px'
-		wrapper.css('background-size', windowWidth + 'px, ' + windowHeight);*/
+		// Adjust the size of the background image to fit the screen size
+		wrapper.css('background-size', windowWidth + 'px, ' + windowHeight + 'px');
+
+		navItem = $('#nav ul li');
+		navItem.on('click', main.onNavClick);
+	},
+	onNavClick: function () {
+		var that, x, navActiveInd;
+		that = $(this);
+		x = that.position().left;
+		navActiveInd = $('#nav_active_ind');
+		navActiveInd.css('left', x);
 	}
 
 
