85ebd2e3c49bd882bee634f80ec7d60faf2618fa
ScottDavey <scottmichaeldavey@gmail.com>
2016-11-16 13:59:01 -0500

    Some animation for the nav

--- script/script.js
+++ script/script.js
@@ -1,25 +1,44 @@
 
 var main = {
 	init: function () {
-		var wrapper, windowHeight, windowWidth, navHeight, navItem;
-		wrapper = $('#wrapper');
-		windowHeight = parseInt($(window).height(), 10);
-		windowWidth = parseInt($(window).width(), 10);
-		// Adjust the size of the background image to fit the screen size
-		wrapper.css('background-size', windowWidth + 'px, ' + windowHeight + 'px');
+		var wrapper, header, screenHeight, navItems, activeNavItem;
 
-		navItem = $('#nav ul li');
-		navItem.on('click', main.onNavClick);
+		// Set background sizing and header margin on load
+		wrapper = $('#wrapper');
+		header = $('#header');
+		screenHeight = screen.height;
+		wrapper.css('background-size', '100%, ' + screenHeight + 'px');
+		header.css('margin-top', (screenHeight / 2) - header.height());
+
+		// Bind an on click event to the nav items
+		navItems = $('.navItem');
+		activeNavItem = navItems.filter('.active');
+		navItems.on('click', main.onNavClick);
+		activeNavItem.trigger('click');
+
+		$(window).on('resize', main.onWindowResize);
 	},
 	onNavClick: function () {
-		var that, x, navActiveInd;
+		var that, x, curActiveItem, navActiveInd;
 		that = $(this);
+		// If we've called this function without a nav item click event, use the current active nav item
+		// if (typeof that === 'object') that = $('.navItem.active');
+		// Get x position of clicked element
 		x = that.position().left;
+		// de-activate old nav item
+		curActiveItem = $('li.active');
+		curActiveItem.removeClass('active');		
+		// Activate clicked nav item
+		that.addClass('active');
+		// Get our active indicator and animate it sliding over to the clicked nav item
 		navActiveInd = $('#nav_active_ind');
-		navActiveInd.css('left', x);
+		navActiveInd.animate({left: [x, 'swing']}, 500, function () { });
+	},
+	onWindowResize: function () {
+		// This function will be responsible for resizing and repositioning everything. Below a certain width, the display will switch to phone/tablet
+		// Since our window has been resized, we need to reposition our nav active indicator
+		main.onNavClick();
 	}
-
-
 };
 
 
