75b5d76ac5255928dfa2f63877174de111961582
ScottDavey <scottmichaeldavey@gmail.com>
2016-11-17 10:43:59 -0500

    Full screen, resizable background image. Fixed nav active indicator on resize.

--- script/script.js
+++ script/script.js
@@ -1,43 +1,57 @@
 
 var main = {
 	init: function () {
-		var wrapper, header, screenHeight, navItems, activeNavItem;
+		var header, screenHeight, navItems, activeNavItem;
 
 		// Set background sizing and header margin on load
-		wrapper = $('#wrapper');
 		header = $('#header');
 		screenHeight = screen.height;
-		wrapper.css('background-size', '100%, ' + screenHeight + 'px');
 		header.css('margin-top', (screenHeight / 2) - header.height());
 
 		// Bind an on click event to the nav items
 		navItems = $('.navItem');
 		activeNavItem = navItems.filter('.active');
-		navItems.on('click', main.onNavClick);
+		navItems.on('click', main.navigation.onNavClick);
 		activeNavItem.trigger('click');
 
 		$(window).on('resize', main.onWindowResize);
+		main.onWindowResize();
 	},
-	onNavClick: function () {
-		var that, x, curActiveItem, navActiveInd;
-		that = $(this);
-		// If we've called this function without a nav item click event, use the current active nav item
-		// if (typeof that === 'object') that = $('.navItem.active');
-		// Get x position of clicked element
-		x = that.position().left;
-		// de-activate old nav item
-		curActiveItem = $('li.active');
-		curActiveItem.removeClass('active');		
-		// Activate clicked nav item
-		that.addClass('active');
-		// Get our active indicator and animate it sliding over to the clicked nav item
-		navActiveInd = $('#nav_active_ind');
-		navActiveInd.animate({left: [x, 'swing']}, 500, function () { });
+	navigation: {
+		onNavClick: function () {
+			var that, x, curActiveItem, navActiveInd;
+			that = $(this);
+			// Get x position of clicked element
+			x = that.position().left;
+			// de-activate old nav item
+			curActiveItem = $('#nav li.active');
+			curActiveItem.removeClass('active');		
+			// Activate clicked nav item
+			that.addClass('active');
+			// Get our active indicator and animate it sliding over to the clicked nav item
+			navActiveInd = $('#nav_active_ind');
+			navActiveInd.animate({left: [x, 'swing']}, 500);
+		}
 	},
 	onWindowResize: function () {
-		// This function will be responsible for resizing and repositioning everything. Below a certain width, the display will switch to phone/tablet
-		// Since our window has been resized, we need to reposition our nav active indicator
-		main.onNavClick();
+		var theWindow, bg, apectRatio, curNavItem, navActiveInd;
+
+		// Get the window and the aspect ratio of the Background
+		theWindow = $(window);
+		bg = $('#bg');
+		aspectRatio = bg.width() / bg.height();
+		
+		// Check to see if the window is greater in width or height. Apply the proper class
+		if ((theWindow.width() / theWindow.height()) < aspectRatio) {
+			bg.removeClass().addClass('bgHeight');
+		} else {
+			bg.removeClass().addClass('bgWidth');
+		}
+
+		// Fix the nav active indicator
+		curNavItem = $('#nav li.active');
+		navActiveInd = $('#nav_active_ind');
+		navActiveInd.css('left', curNavItem.position().left);
 	}
 };
 
